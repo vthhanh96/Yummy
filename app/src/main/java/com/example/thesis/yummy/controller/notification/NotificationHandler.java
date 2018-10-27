462e99d68180ce5c4279ee1896ee2eb0b928289d
havh <honghaa8@gmail.com>
2018-10-27 16:28:03 +0700

    update notification

--- app/src/main/java/com/example/thesis/yummy/controller/notification/NotificationHandler.java
+++ app/src/main/java/com/example/thesis/yummy/controller/notification/NotificationHandler.java
@@ -1,12 +1,25 @@
 package com.example.thesis.yummy.controller.notification;
 
 import android.content.Context;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 
+import com.example.thesis.yummy.R;
 import com.google.gson.JsonObject;
 
 public class NotificationHandler {
 
+    private static final String NOTIFICATION_CHANNEL_ID = "NOTIFICATION_CHANNEL_ID";
+
     public static void createNotification(Context context, JsonObject notificationData) {
-        
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_notification)
+                .setContentTitle(context.getString(R.string.app_name))
+                .setContentText("Someone reaction your post")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        notificationManager.notify(100, builder.build());
+
     }
 }
